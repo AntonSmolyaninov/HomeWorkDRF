3bdd962d0fe452b12cc37c6e2d99c8c70560803b
Anton Smolyaninov <antonsmol77@gmail.com>
2026-03-30 23:38:27 +1100

    chang views and serializers

--- users/serializers.py
+++ users/serializers.py
@@ -1,3 +1,4 @@
+from django.db import models
 from rest_framework import serializers
 from .models import User, Payment
 from materials.serializers import CourseSerializer, LessonSerializer
--- users/views.py
+++ users/views.py
@@ -1,4 +1,3 @@
-# users/views.py
 from rest_framework import viewsets, permissions, filters
 from django_filters.rest_framework import DjangoFilterBackend
 from .models import User, Payment
@@ -9,23 +8,6 @@ from .filters import PaymentFilter
 class PaymentViewSet(viewsets.ModelViewSet):
     """
     ViewSet для управления платежами.
-
-    Поддерживает:
-    - Сортировку по дате оплаты (ordering=-payment_date, ordering=payment_date)
-    - Фильтрацию по курсу (course=1)
-    - Фильтрацию по уроку (lesson=1)
-    - Фильтрацию по способу оплаты (payment_method=cash или payment_method=transfer)
-    - Фильтрацию по пользователю (user=1)
-    - Фильтрацию по дате (payment_date_from=2024-01-01, payment_date_to=2024-12-31)
-    - Фильтрацию по сумме (amount_min=1000, amount_max=5000)
-
-    Примеры запросов:
-    GET /api/users/payments/?ordering=-payment_date
-    GET /api/users/payments/?course=1
-    GET /api/users/payments/?lesson=2
-    GET /api/users/payments/?payment_method=cash
-    GET /api/users/payments/?course=1&payment_method=transfer
-    GET /api/users/payments/?ordering=-payment_date&course=1
     """
     queryset = Payment.objects.all()
     serializer_class = PaymentSerializer
@@ -50,9 +32,6 @@ class PaymentViewSet(viewsets.ModelViewSet):
 class UserPaymentViewSet(viewsets.ReadOnlyModelViewSet):
     """
     ViewSet для просмотра пользователей с их платежами.
-
-    GET /api/users/user-payments/ - список пользователей с платежами
-    GET /api/users/user-payments/{id}/ - детали пользователя с его платежами
     """
     queryset = User.objects.all()
     serializer_class = UserPaymentSerializer
